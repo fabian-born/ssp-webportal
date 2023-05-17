0f2fb036d0cec951eaba1d62676df802404abb8b
Fabian Born <fabian.born@fabianborn.net>
2023-05-17 08:18:56 +0000

    Add Dockerfile, remove configs

--- app/login/config-.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'mysql');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'selfservice');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
