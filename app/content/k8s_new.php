d886c0d62d6819556f263d4a7846514a65cc7b29
Fabian Born <fabian.born@fabianborn.net>
2023-06-18 12:12:43 +0000

    adding k8s support

--- app/content/k8s_new.php
+++ app/content/k8s_new.php
@@ -10,8 +10,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "SELECT uid FROM users WHERE username = '$login_session'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    echo "INSERT INTO k8s (kid,owner,clustername,state,kubeconfig) VALUES (NULL, " . $row['uid'] . ",\"" . $_POST['clustername'] . "\" ,\"deploying\", NULL);";
-    $update_sql = mysqli_query($db,"INSERT INTO k8s (kid,owner,clustername,state,kubeconfig) VALUES (NULL, " . $row['uid'] . ",\"" . $_POST['clustername'] . "\" ,\"deploying\", NULL);");
+    echo "INSERT INTO k8s (kid,owner,clustername,state,kubeconfig) VALUES (NULL, " . $row['uid'] . ", " . $_POST['clustername'] . " ,\"deploying\", NULL);";
+    $update_sql = mysqli_query($db,"INSERT INTO k8s (kid,owner,clustername,state,kubeconfig) VALUES (NULL, " . $row['uid'] . ", " . $_POST['clustername'] . " ,\"deploying\", NULL);");
 
     // echo $json_body;
     # send_apicall2awx($config["aap_jid_createvm"] , $json_body);
