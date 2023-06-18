97d0818534820562bc4e7e3eeef9964d8896abc8
Fabian Born <fabian.born@fabianborn.net>
2023-06-18 14:53:46 +0000

    adding k8s support

--- app/content/k8s_inc.php
+++ app/content/k8s_inc.php
@@ -11,7 +11,7 @@ if ($_REQUEST["sub"] == "overview"){  $overview_active = "active"; }
 ?>
 
 <div class="container-fluid">
-      <h2><?php echo $pageTitle; ?></h2>
+      <h2><img src="https://banner2.cleanpng.com/20180330/rqq/kisspng-kubernetes-docker-software-deployment-orchestratio-engine-5abe1b5d5ce3d2.7155767715224082853805.jpg"><?php echo $pageTitle; ?></h2>
       <hr>
       <div class="row">
         <div class="form-group col-md-12">
